4726fcce256e6e4328cee9e128689c1e80290aa8
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-15 19:57:19 +0300

    fddffdfd

---
+++ compare-files.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+function compareKeys(file1, file2) {
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < keys1.length; i++) {
+    const key = keys1[i];
+    if (!keys2.includes(key)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const uaFile = require("./src/i18n/ua/chat.js");
+const enFile = require("./src/i18n/en/chat.js");
+
+if (compareKeys(uaFile, enFile)) {
+  console.log("All keys are correct.");
+} else {
+  console.log("Keys are not the same in all files.");
+}
