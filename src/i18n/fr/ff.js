e8c2e968b4a355a64c4c353e5c365884415e73f0
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-15 21:39:13 +0300

    fddffdfd

---
+++ src/i18n/fr/ff.js
@@ -0,0 +1,56 @@
+export default {
+  message: "Message",
+  messages_not_avaliable: "Messages non disponibles",
+  write_message: "Écrire un message...",
+  you_were_kicked_from_the_chat_small:
+    "Désolé, mais vous avez été exclu du groupe. L'envoi de messages dans ce chat n'est plus disponible",
+  you_were_kicked_from_the_chat:
+    "Désolé, mais vous avez été exclu du groupe. L'envoi de messages dans ce chat n'est plus possible. Vous pouvez contacter l'administration du canal pour plus d'informations",
+  select_chat_from_the_list: "Choisissez un chat pour commencer à discuter",
+  user_joined_to_chat: "{userFullName} vient de rejoindre la conversation",
+  membres: "Membres",
+  online: "En ligne",
+  message_edited: "edit",
+  message_editing: "Modifier le message",
+  emoji_groups: {
+    smileys_people: "Sourires et personnes",
+    animals_nature: "Animaux et nature",
+    food_drink: "Nourriture et boissons",
+    activités: "Activités",
+    travel_places: "Lieux de voyage",
+    objets: "Objets",
+    symboles: "Symboles",
+    drapeaux: "Drapeaux",
+  },
+  no_chat_messages: {
+    title: "Il n'y a pas encore eu de notifications dans ce chat",
+    main_text:
+      "Envoyez un message au premier pour entamer la conversation maintenant !",
+  },
+  edit_chat_modal: {
+    select_photo: "Choisissez une photo",
+    manage_group: "Gestion du groupe",
+    nom_du_groupe_chat: "Titre de la conversation",
+    invitation_link: "Lien d'invitation",
+  },
+  submit_close_edit_chat_modal: {
+    title: "Unedit group chat",
+    description: "Êtes-vous sûr de vouloir annuler toutes vos modifications ?",
+    button_1: "Non, n'annulez pas",
+    bouton_2: "Oui, annuler",
+  },
+  admins_limit_reached_modal: {
+    title: "Dépassement de la limite",
+    main_text: "Vous pouvez assigner jusqu'à 3 modérateurs",
+  },
+  toasts: {
+    chat_link_copieded_success:
+      "Le lien d'invitation au chat a été copié avec succès dans le presse-papiers",
+    chat_updated_success:
+      "Les données de ce chat de groupe ont été modifiées avec succès",
+  },
+  rôles: {
+    admin: "Admin",
+    author: "Organisateur",
+  },
+};
