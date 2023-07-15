5a4a1b57b29d8f67a660e8bedc344771cbd04071
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-15 20:26:54 +0300

    dffddffd

--- compare-locales-files.js
+++ compare-locales-files.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const path = require("path");
 
 function compareKeys(file1, file2) {
+  console.log(file1);
+  console.log(file2);
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
 
