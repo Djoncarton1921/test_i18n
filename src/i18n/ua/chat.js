973bed187f9a202290f47ccba421c61749c051e5
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-15 20:14:48 +0300

    dfdfddf

--- src/i18n/ua/chat.js
+++ src/i18n/ua/chat.js
@@ -32,7 +32,7 @@ export default {
     name_of_group_chat: "Назва бесіди",
     invitation_link: "Посилання-запрошення",
   },
-  submit_close_edit_chat_modal: {
+  12121212: {
     title: "Скасувати редагування групового чату",
     description: "Ви дійсно хочете скасувати всі внесені зміни?",
     button_1: "Ні, не скасосувати",
