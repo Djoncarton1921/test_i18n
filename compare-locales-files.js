c6fb59986baa80606ecfd8e65ea689f724c3dd49
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-15 20:11:27 +0300

    dfdffddf

--- compare-locales-files.js
+++ compare-locales-files.js
@@ -9,21 +9,27 @@ function compareKeys(file1, file2) {
     return false;
   }
 
+  const differingKeys = [];
+
   for (let i = 0; i < keys1.length; i++) {
     const key = keys1[i];
     if (!keys2.includes(key)) {
-      return false;
+      differingKeys.push(key);
     }
   }
 
-  return true;
+  return differingKeys.length === 0 ? true : differingKeys;
 }
 
 const uaFile = require("./src/i18n/ua/chat.js").default;
 const enFile = require("./src/i18n/en/chat.js").default;
 
-if (compareKeys(uaFile, enFile)) {
-  console.log("All keys are correct.");
+const differingKeys = compareKeys(uaFile, enFile);
+
+if (differingKeys === true) {
+  throw new Error("No differences in locales were found");
 } else {
-  throw new Error("Keys are not the same in all files.");
+  console.log(
+    `There are differences between locale files. Differences in such keys as: ${differingKeys}`
+  );
 }
