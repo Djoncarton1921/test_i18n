3e889c8c2d51ae2bdacb4acb0c9ae65e1fdf5e2b
GitHub Action <action@github.com>
2023-07-17 09:54:22 +0000

    Update translations

---
+++ src/i18n/fr/ff.js
@@ -0,0 +1,64 @@
+Exporter par défaut {
+  message: « Messages »,
+  messages_not_avaliable : « Messages non disponibles »,
+  write_message: « Ecrire un message... »,
+  you_were_kicked_from_the_chat_small :
+    « Je suis désolé, mais vous avez été exclu du groupe. L’envoi de messages dans ce chat n’est plus disponible. »,
+  you_were_kicked_from_the_chat :
+    « Je suis désolé, mais vous avez été exclu du groupe. L’envoi de messages dans ce chat n’est plus disponible. Vous pouvez contacter l’administration du canal pour plus d’informations »,
+  select_chat_from_the_list: « Sélectionnez le chat pour commencer à discuter »,
+  user_joined_to_chat: « {userFullName} vient de rejoindre la conversation »,
+  membres : « Contributeurs »,
+  en ligne : « En ligne »,
+  message_edited: « éd. »,
+  message_editing : Modifier un message,
+  emoji_groups: {
+    smileys_people : « Des sourires et des gens »,
+    animals_nature : « Animaux et nature »,
+    food_drink: « Nourriture et boissons »,
+    activités: « Activités »,
+    travel_places: « Lieux de voyage »,
+    objets: « Objets »,
+    symboles: « Symboles »,
+    drapeaux: « Drapeaux »,
+  },
+  no_chat_messages: {
+    titre: « Il n’y avait pas encore de messages dans ce chat »,
+    main_text: « Envoyez d’abord un message pour entamer une conversation maintenant! »,
+  },
+  edit_chat_modal: {
+    select_photo: « Choisir une photo »,
+    manage_group : Gérer un groupe,
+    name_of_group_chat : « Titre de la conversation »,
+    invitation_link : « Lien d’invitation »,
+  },
+  submit_close_edit_chat_modal: {
+    titre: « Unedit Group Chat »,
+    description: « Voulez-vous vraiment annuler toutes vos modifications? »,
+    button_1: « Non, ne pas annuler »,
+    button_2: « Oui, annuler »,
+  },
+  admins_limit_reached_modal: {
+    titre: « Dépassement de la limite »,
+    main_text : « Vous pouvez affecter jusqu’à 3 modérateurs. »,
+  },
+  Toasts: {
+    chat_link_copieded_success :
+      « Le lien d’invitation au chat a été copié avec succès dans le presse-papiers »,
+    chat_updated_success: « Les données de ce chat de groupe ont été modifiées avec succès »,
+  },
+  Rôles: {
+    admin: « Admin »,
+    auteur: « Organisateur »,
+  },
+  boutons: {
+    set_admin: « Faire admin »,
+    unset_admin: « Ramasser l’administrateur »,
+    enable_push_notifications: « Activer les notifications »,
+    disable_push_notifications : Désactiver les notifications,
+    leave_group : « Quitter le groupe »,
+    delete_chat: Supprimer le chat,
+    search_messages : Recherche de messages,
+    manage_group : Gérer un groupe,
+  },
+};
