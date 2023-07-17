e2ee96afe8d761ae26e31ab50aa241c305781803
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-17 12:50:18 +0300

    fddffdfd

--- src/i18n/ua/ff.js
+++ src/i18n/ua/ff.js
@@ -1,1 +1,64 @@
-export default{message:"Message",messages_not_avaliable:"Messages not available",write_message:"Write a message...",you_were_kicked_from_the_chat_small:"Sorry, but you were excluded from the group. Sending messages in this chat is no longer available.",you_were_kicked_from_the_chat:"Sorry, but you were excluded from the group. Sending messages in this chat is no longer available. You can contact the channel administration for more information",select_chat_from_the_list:"Choose a chat to start chatting",user_joined_to_chat:"{userFullName} just joined the conversation",members:"Members",online:"Online",message_edited:"edit",message_editing:"Edit message",emoji_groups:{smileys_people:"Smiles and people",animals_nature:"Animals and nature",food_drink:"Food and drink",activities:"Activities",travel_places:"Places to travel",objects:"Objects",symbols:"Symbols",flags:"Flags",},no_chat_messages:{title:"There were no notifications in this chat yet",main_text:"Send a message to the first to start the conversation now!",},edit_chat_modal:{select_photo:"Choose photo",manage_group:"Group management",name_of_group_chat:"Conversation title",invitation_link:"Invitation link",},submit_close_edit_chat_modal:{title:"Unedit group chat",description:"Are you sure you want to undo all your changes?",button_1:"No, do not cancel",button_2:"Yes, cancel",},admins_limit_reached_modal:{title:"Exceeding the limit",main_text:"You can assign up to 3 moderators.",},toasts:{chat_link_copieded_success:"The chat invitation link was successfully copied to the clipboard",chat_updated_success:"The data of this group chat has been changed successfully",},roles:{admin:"Admin",author:"Organizer",},buttons:{set_admin:"Make admin",unset_admin:"Pick up the admin",enable_push_notifications:"Turn on notifications",disable_push_notifications:"Turn off notifications",leave_group:"Leave the group",delete_chat:"Delete chat",search_messages:"Search for messages",manage_group:"Group management"}}
+export default {
+  message: "Повідомлення",
+  messages_not_avaliable: "Повідомлення недоступні",
+  write_message: "Написати повідомлення...",
+  you_were_kicked_from_the_chat_small:
+    "Шкода, але вас було виключено із групи. Відправка повідомлень у цьому чаті більше недоступна.",
+  you_were_kicked_from_the_chat:
+    "Шкода, але вас було виключено із групи. Відправка повідомлень у цьому чаті більше недоступна. Ви можете звернутися до адміністрації каналу за додатковою інформацією",
+  select_chat_from_the_list: "Оберіть чат, щоб розпочати спілкування",
+  user_joined_to_chat: "{userFullName} щойно долучився до бесіди",
+  members: "Учасники",
+  online: "У мережі",
+  message_edited: "ред.",
+  message_editing: "Редагування повідомлення",
+  emoji_groups: {
+    smileys_people: "Посмішки та люди",
+    animals_nature: "Тварини та природа",
+    food_drink: "Їжа та напої",
+    activities: "Діяльності",
+    travel_places: "Місця для подорожей",
+    objects: "Об`єкти",
+    symbols: "Символи",
+    flags: "Прапори",
+  },
+  no_chat_messages: {
+    title: "У цьому чаті ще не було повідомлень",
+    main_text: "Надішліть повідомлення першим, щоб розпочати бесіду вже зараз!",
+  },
+  edit_chat_modal: {
+    select_photo: "Обрати фото",
+    manage_group: "Керування групою",
+    name_of_group_chat: "Назва бесіди",
+    invitation_link: "Посилання-запрошення",
+  },
+  submit_close_edit_chat_modal: {
+    title: "Скасувати редагування групового чату",
+    description: "Ви дійсно хочете скасувати всі внесені зміни?",
+    button_1: "Ні, не скасосувати",
+    button_2: "Так, скасувати",
+  },
+  admins_limit_reached_modal: {
+    title: "Перевищення ліміту",
+    main_text: "Ви можете призначити не більше 3 модераторів.",
+  },
+  toasts: {
+    chat_link_copieded_success:
+      "Посилання-запрошення на чат було успішно скопійовано в буфер обміну",
+    chat_updated_success: "Дані цього групового чату було успішно змінено",
+  },
+  roles: {
+    admin: "Адмін",
+    author: "Організатор",
+  },
+  buttons: {
+    set_admin: "Зробити адміном",
+    unset_admin: "Забрати адміна",
+    enable_push_notifications: "Увімкнути сповіщення",
+    disable_push_notifications: "Вимкнути сповіщення",
+    leave_group: "Покинути групу",
+    delete_chat: "Видалити чат",
+    search_messages: "Пошук повідомлень",
+    manage_group: "Керування групою",
+  },
+};
