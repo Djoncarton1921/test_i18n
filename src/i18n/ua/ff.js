91db82f08b2d7694fc94fdf3766c5ca2ea0209b5
Djoncarton1921 <shapranov.nik@gmail.com>
2023-07-17 10:54:12 +0300

    cdffffffffff

--- src/i18n/ua/ff.js
+++ src/i18n/ua/ff.js
@@ -2,46 +2,5 @@ export default {
   buttons: {
     set_admin: "Зробити адміном",
   },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
-  buttons: {
-    set_admin: "Зробити адміном",
-  },
+  efdfdfdfd: "dfffffffff",
 };
